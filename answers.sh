a2702f24246dd9673844b4a1a96dcb5164baf737
Jayden Cox <jayden_cox@tamu.edu>
2024-10-04 08:16:27 -0500

    Count and Filter

--- answers.sh
+++ answers.sh
@@ -23,8 +23,8 @@ SELECT employees.name, employees.Company, companies.date FROM employees Inner jo
 SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
 SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
 
-select name from students where points=(SELECT MAX(points) from students);
-select AVG(points) from students;
-select COUNT(Name) from students where points=500;
-select Name from students WHERE Name GLOB '*s*';
-select name from students ORDER BY points DESC;
+SELECT name FROM students WHERE points=(SELECT MAX(points) from students);
+SELECT AVG(points) FROM students;
+SELECT COUNT(Name) FROM students WHERE points=500;
+SELECT Name FROM students WHERE Name GLOB '*s*';
+SELECT name FROM students ORDER BY points DESC;
